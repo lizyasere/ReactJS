79574a548a20f03dd58b9198400de21965e2b1c3
lizyasere <iyasere.elizabeth@gmail.com>
2023-07-06 10:37:47 +0100

    modified some component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,22 @@
-import ExpenseDate from './ExpenseDate';
+import ExpenseDate from "./ExpenseDate";
 
-import './ExpenseItem.css';
-
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    // const expenseDate = new Date(2023, 7, 5);
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 294.67;
-    
+  // const expenseDate = new Date(2023, 7, 5);
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = 294.67;
 
-    return (
-        <div className="expense-item">
-           <ExpenseDate date={props.date} />
-            <div>{props.date.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-        </div>
-    );
-    
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
